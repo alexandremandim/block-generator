5ae104913fc58c0cc83bead518414cb9030e637a
Alexandre Silva <mandimagc@gmail.com>
2019-04-18 04:30:56 +0100

    Implementing precise compression between 2 blocks

--- main.cpp
+++ main.cpp
@@ -22,25 +22,11 @@ class Generator {
         tuple<double, double> compressions_inter_blocks_interval;
         vector<Linha> linhas;
         vector<double> weights;
-        vector<unsigned char*> modelos;
+        vector<vector<unsigned char*>> modelos;     /* Vector com 100 modelos  Cada modelo tem 10 blocos (compressao 0, 10, 20 , ..., 100) */
+        vector<int> atribuicao_blocos_unicos_para_modelos; /* Vetor com 100 posicoes. Cada posicao representa o numero de blocos desse modelo */
 
         /* Returns random block ID from a line */
         int getRandomBlockFromLine(Linha l) {
-
-            if (l.nrCopies == 0) {
-                int r = rand();
-                return (r);
-            } else {
-                random_device rd; //Will be used to obtain a seed for the random number engine
-                mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
-                uniform_int_distribution<> dis(1, l.nrBlocks);
-
-                return dis(gen);
-            }
-        }
-
-        /* Returns random block ID from a line */
-        int getRandomBlockFromLine2(Linha l) {
 
             random_device rd; //Will be used to obtain a seed for the random number engine
             mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
@@ -63,35 +49,6 @@ class Generator {
         /* Returns compression index from a block */
         int giveMyCompression(Linha linhaAleatoria, int randomBlockID) {
 
-            /* Como o ID é completamente aleatório, também a compressão vai ser com base nas percentagens */
-            if(linhaAleatoria.nrCopies == 0){
-                /* Get random compression */
-                unsigned seedDistr = std::chrono::system_clock::now().time_since_epoch().count();
-                std::default_random_engine generator(seedDistr);
-                discrete_distribution<> distribution(linhaAleatoria.compression.begin(), linhaAleatoria.compression.end());
-                /* Devolve um indice (0-9)*/
-                int r = distribution(generator);
-                return r;
-            }
-            
-            /* Cada bloco tem uma compressão ligada a ele */
-            else{
-                double myPercent = ((double) randomBlockID) / linhaAleatoria.nrBlocks * 100;
-                double aux = 0;
-
-                for (int i = 0; i < 10; i++) {
-                    aux += linhaAleatoria.compression.at(i);
-                    if (myPercent <= aux) {
-                        return i;
-                    }
-                }
-                return 9;
-            }
-        }
-
-        /* Returns compression index from a block */
-        int giveMyCompression2(Linha linhaAleatoria, int randomBlockID) {
-
             double myPercent = ((double) randomBlockID) / linhaAleatoria.nrBlocks * 100;
             double aux = 0;
 
@@ -106,9 +63,18 @@ class Generator {
         
         /* Fill buffer with data with a blockID and compression */
         void generate_data(unsigned char* buffer, int blockKey, int compression){
-            
-            unsigned char* bufferModelo = modelos.at(compression);
-            
+
+            int indice = 0, somatorio_ids = this->atribuicao_blocos_unicos_para_modelos.at(indice);
+
+            while(blockKey > somatorio_ids && indice < this->atribuicao_blocos_unicos_para_modelos.size()){
+                indice++;
+                somatorio_ids += this->atribuicao_blocos_unicos_para_modelos.at(indice);
+            }
+            if(indice == this->atribuicao_blocos_unicos_para_modelos.size()) indice = 0; /* ERRO !!!Nunca deve entrar nesta condição */   
+
+
+            unsigned char* bufferModelo = modelos.at(indice).at(compression);
+
             /* Copiar o buffer (Posso otimizar este passo para ser mais rapido).
              * Em vez de copiar insiro o blockKey no bufferModelo e retorno-o */
             for(int i = 0; i < blockSize; i ++)
@@ -120,59 +86,6 @@ class Generator {
             
         }
         
-        /* Auxiliar Function to Generate Block Models*/
-        static void generate4096BytesBlock(unsigned char* buffer, double seed){
-            
-            double c = 1;
-            
-            /* Gerar o bloco */
-            for(int contador = 0; contador < 4096/sizeof(c); contador++){ 
-                for (size_t i = 0; i < sizeof(c); ++i) 
-                  buffer[(sizeof(c)*contador)+i] = *((unsigned char *)&c + i);
-                c = c * seed;
-            }
-        }
-
-        /* Returns a block with 0% compression */
-        unsigned char* generateBlockWithNoCompression(int blockSize, double seed){
-            if(blockSize < 4096) return NULL;
-
-            int nrBlocos4096 = blockSize/4096;
-            int tamanhoTotal = nrBlocos4096*4096;
-
-            unsigned char *buffer = (unsigned char *)malloc(sizeof(unsigned char)*(tamanhoTotal));
-
-            /* Gerar alguma aleatoriedade */
-            for(int i = 0; i< nrBlocos4096; i++)
-                generate4096BytesBlock(&(buffer[i*4096]), seed);
-
-            return buffer;
-        }
-
-        /* This function transforms a block with no compression to compress 'rate'%
-         * rate is the rate compression (1 - 100) */
-        void transformBlockToCompress(unsigned char* buffer, int rate, int tamanhoBuffer){
-
-            if(rate < 1 || rate > 100 || tamanhoBuffer <= 0 || buffer == NULL) return;
-            rate = rate + 0;
-            /* Tamanho do semi-bloco a ser repetido (1% do tamanho total) */
-            int nrBlocosADuplicar = tamanhoBuffer/100, posicaoBuffer = 0, contador = 0;
-
-            /* Começa na posicao 0 do buffer, vai andar de 1 em 1 % do buffer. 
-             * Vai copiar os primeiros 1% dos bytes até à taxa pretendida. Ex: se a taxa for 40% vai copiar
-             os primeiros 1% 40 vezes
-            */
-
-            while(contador < rate && posicaoBuffer < (tamanhoBuffer-nrBlocosADuplicar)){
-                for(int posicaoAuxiliar = 0; posicaoAuxiliar < nrBlocosADuplicar; posicaoAuxiliar++)
-                    buffer[posicaoBuffer + posicaoAuxiliar] = buffer[posicaoAuxiliar];
-
-                posicaoBuffer = posicaoBuffer + nrBlocosADuplicar;
-                contador++;
-            }
-
-        }
-
         /* Returns a block with a given size and compression. 
          * Compression must be between 1 and 99. 
          * The blockSize must be multiple of 4096
@@ -181,46 +94,97 @@ class Generator {
 
             if(compression < 1 || compression > 99) return NULL;
             int size = blockSize/4096; size = size * 4096; /* blockSize must be multiple of 4096 */
+            int nrBlocos4096 = blockSize/4096;
+            unsigned char *buffer = (unsigned char *)malloc(sizeof(unsigned char)*(size));  /* Generate block with 0% compression */
 
-            unsigned char* buffer = generateBlockWithNoCompression(size, seed);
-            if(buffer != NULL) transformBlockToCompress(buffer,compression,size);
+            
 
+            /* Preencher o buffer com data random com 0% de compressão */
+            for(int i = 0; i< nrBlocos4096; i++){
+                double c = 1;
+        
+                /* Gerar o bloco */
+                for(int contador = 0; contador < 4096/sizeof(c); contador++){ 
+                    for (size_t i = 0; i < sizeof(c); ++i) 
+                    buffer[(sizeof(c)*contador)+i] = *((unsigned char *)&c + i);
+                    c = c * seed;
+                }
+            }
+
+
+            /* This code transforms a block with no compression to compress 'rate'%
+            * rate is the rate compression (1 - 100) */
+            if(buffer != NULL){
+
+                if(compression < 1 || compression > 100 || size <= 0 || buffer == NULL) return NULL;
+                compression = compression + 0;
+                /* Tamanho do semi-bloco a ser repetido (1% do tamanho total) */
+                int nrBlocosADuplicar = size/100, posicaoBuffer = 0, contador = 0;
+
+                /* Começa na posicao 0 do buffer, vai andar de 1 em 1 % do buffer. 
+                * Vai copiar os primeiros 1% dos bytes até à taxa pretendida. Ex: se a taxa for 40% vai copiar
+                os primeiros 1% 40 vezes
+                */
+
+                while(contador < compression && posicaoBuffer < (size-nrBlocosADuplicar)){
+                    for(int posicaoAuxiliar = 0; posicaoAuxiliar < nrBlocosADuplicar; posicaoAuxiliar++)
+                        buffer[posicaoBuffer + posicaoAuxiliar] = buffer[posicaoAuxiliar];
+
+                    posicaoBuffer = posicaoBuffer + nrBlocosADuplicar;
+                    contador++;
+                }
+
+            }
             return buffer;
         }
         
         /* 
-         * different_nc_ratio: ratio of blocks with different non-compressive areas needed
-         *                     to achive wanted media
+         * nr_model_vectors: nr of vectors loaded
          * return: 1 -> ok, -1 -> error 
          */
-        int loadModels(int different_nc_ratio){
+        int loadModels(int percentagem_compressao_entre_blocos){
 
-           /* TODO: Gerar outros modelos com ares de não compressão diferentes, 
-           de forma a tornar different_nc_ratio do dataset diferente. */
-            
+            int nr_models = 100;
 
-            cout << "Creating models..." << endl;
+            cout << "Creating  " << nr_models << "  model(s)..." << endl;
             srand (time ( NULL));
+            
+            /* Gerar todos os vetores de modelos para memória */
+            for(int vectores_created = 0; vectores_created < nr_models; vectores_created++){
+                /* Gerar um valor random (double) que vai ser utilizado para gerar um bloco sem compressão, 
+                através da multiplicação sucessivada deste valor */
+                /* Neste caso, como geramos apenas uma seed, todos os modelos vão ser iguais na parte que não comprime */
 
-            /* Gerar um valor random (double) que vai ser utilizado para gerar um bloco sem compressão, 
-            através da multiplicação sucessivada deste valor */
-            /* Neste caso, como geramos apenas uma seed, todos os modelos vão ser iguais na parte que não comprime */
+                /* Gerar Seed */
+                double max = 1.1; double min = 1.9;
+                double seed =  min + (rand() / (RAND_MAX / (max - min)));
+                vector<unsigned char*> new_vector;
 
-            double max = 1.1; double min = 1.9;
-            double seed =  min + (rand() / (RAND_MAX / (max - min)));
+                //cout << "Seed " << seed << " para o vector " << vectores_created << endl;
 
-            for(int compression = 5; compression <= 95; compression=compression+10){        
-
-                unsigned char *buffer = blockModel(this->blockSize, compression, seed);
-                
-                if(buffer != NULL)
-                    this->modelos.push_back(buffer);
-                else {
-                    cout << "Error loading models..." << endl;
-                    return -1;
+                /* Gerar vetor de 10 modelos */
+                for(int compression = 5; compression <= 95; compression=compression+10){        
+                    unsigned char *buffer = blockModel(this->blockSize, compression, seed);
+    
+                    if(buffer != NULL)  new_vector.push_back(buffer);      /* Adicionar modelo ao vector */
+                    else {
+                        cout << "Error loading models..." << endl;
+                        return -1;
+                    }
                 }
+                modelos.push_back(new_vector);             /* Adicionar vector de modelos ao vector com os vectores */
             }
-            cout << "Models created with success." << endl << endl;
+
+            /* Atribuir os modelos a blocos */
+            int bu = this->total_unique_blocks;
+            double p = (double)percentagem_compressao_entre_blocos / (double)100;
+            cout << "Trying to achive " << p*(get<1>(this->compressions_inter_blocks_interval) - get<0>(this->compressions_inter_blocks_interval)) << " compression between 2 random blocks." << endl;
+            int x = sqrt(p) * bu, r = bu - x, y = r / (nr_models - 1), z = r % (nr_models - 1), w = (nr_models - 1) - z;
+            for(int i = 0; i < 1; i++)  this->atribuicao_blocos_unicos_para_modelos.push_back(x);
+            for(int i = 0; i < w; i++)  this->atribuicao_blocos_unicos_para_modelos.push_back(y);
+            for(int i = 0; i < z; i++)  this->atribuicao_blocos_unicos_para_modelos.push_back(y+1);
+
+            cout << "Models created with success." << endl << endl;            
             return 1;
         }
 
@@ -236,7 +200,7 @@ class Generator {
                 id_line = block_id - it->nrBase;
 
                 if(block_id >= min_id_line && block_id <= max_id_line){ /* É esta a linha do bloco */
-                    int x = giveMyCompression2(*it, id_line);
+                    int x = giveMyCompression(*it, id_line);
                     return x * 10;
                 }
             }
@@ -274,7 +238,7 @@ class Generator {
                     minimum += new_media_min; maximum += new_media_max;
                 }
             }
-            cout << "Values estimated based on " << nr_pairs << " pairs." << endl;
+            cout << "Compression interval estimated based on " << nr_pairs << " pairs of block." << endl;
 
             return make_tuple(minimum/nr_pairs, maximum/nr_pairs);
         }
@@ -286,8 +250,8 @@ class Generator {
             Linha linhaAleatoria = getLinha();
             int myCompression = 0;
 
-            int randomBlockID = getRandomBlockFromLine2(linhaAleatoria);
-            myCompression = giveMyCompression2(linhaAleatoria, randomBlockID);
+            int randomBlockID = getRandomBlockFromLine(linhaAleatoria);
+            myCompression = giveMyCompression(linhaAleatoria, randomBlockID);
             
             int blockKey = randomBlockID + linhaAleatoria.nrBase;
             generate_data(buffer, blockKey, myCompression);
@@ -301,8 +265,9 @@ class Generator {
          * percentage: percetage of unique blocks used to estimate compression inter blocks
          * Return 1:ok -1:error
         */
-        int initialize(string path, int block_Size, int nrBlocksToGenerate, int percentage, int wanted_media_compression_inter_blocks) {
-            if(block_Size < 4096) return -1;
+        int initialize(string path, int block_Size, int nrBlocksToGenerate, int percentage, int percentagem_compressao_entre_blocos) {
+            if(block_Size < 4096) return -1;                                                                     
+            if(!(percentagem_compressao_entre_blocos >= 1 && percentagem_compressao_entre_blocos <= 100)) return -1; /* Variavel tem q estar no intervalo [1,100] */
 
             int nrBlocos4096 = block_Size/4096;
             this->blockSize = nrBlocos4096*4096;
@@ -359,7 +324,7 @@ class Generator {
             cout << "Unique blocks:\t" << this->total_unique_blocks << endl << endl;
 
             /* Calcular o intervalo esperado de compressão entre blocos*/
-            cout << "Estimating interval of compression between two blocks" << endl;
+            //cout << "Estimating interval of compression between two blocks" << endl;
             compressions_inter_blocks_interval = get_media_inter(percentage);
             cout << "Estimated Compression Interval: [" << get<0>(compressions_inter_blocks_interval) 
                 << ", " << get<1>(compressions_inter_blocks_interval) << "]" << endl << endl;
@@ -367,34 +332,27 @@ class Generator {
             /* Check if wanted media belongs to inverval */
             double infLimit = get<0>(this->compressions_inter_blocks_interval);
             double supLimit = get<1>(this->compressions_inter_blocks_interval);
-            if(wanted_media_compression_inter_blocks < infLimit){       /* Wanted media not in interval */
-                wanted_media_compression_inter_blocks = infLimit;
-            }
-            else if(wanted_media_compression_inter_blocks > supLimit){  /* Wanted media not in interval */
-                wanted_media_compression_inter_blocks = supLimit;
-            }
 
-            /* Calculating different_ratio used to change model blocks */
-            int different_ratio = (wanted_media_compression_inter_blocks - infLimit) / (supLimit - infLimit); /* ratio of different non-compression areas needed to achive wanted media */
             /* Generating Block Models */
-            int returnLoadModels = loadModels(different_ratio);
+            /* TODO: enviar o parametro correto */
+            int returnLoadModels = loadModels(percentagem_compressao_entre_blocos);
 
             /* TODO: Por fim, associar modelos aos blocos., alterar geração */
     
             return(returnLoadModels);
         }
-        
 };
 
 int main(int argc, char ** argv){
     
-    const int blockSize = 4096, blocosAGerar = 416830;
-    const int wanted_media = 50, percentage_unique_blocks_analyze = 5;
+    const int blockSize = 4096, blocosAGerar = 100000;
+    const int percentage_unique_blocks_analyze = 5;
+    const int percentagem_compressao_entre_blocos = 75; /* Variavel entre 1 e 100. 1 Significa q deve comprimir o minimo possivel entre 2 blocos, 100 o máximo */
     Generator generator;
     string pathToWrite = "/home/alexandre/Desktop/gerado/data", pathToRead = "./input.txt";
 
     if (generator.initialize(pathToRead, blockSize, blocosAGerar, 
-        percentage_unique_blocks_analyze, wanted_media) == 1) {
+        percentage_unique_blocks_analyze, percentagem_compressao_entre_blocos) == 1) {
         unsigned char *buffer = new unsigned char[blockSize]();
         FILE *write_ptr = fopen(pathToWrite.c_str(),"wb+");
 
